6a782f483f416223b0cd8c78df8467816b6f9906
bthcode <brianhone@gmail.com>
2024-08-18 13:55:02 -0400

    basic ui functional

--- roguelib/ui.py
+++ roguelib/ui.py
@@ -53,7 +53,7 @@ class GameUI():
 
         self.game_win = curses.newwin(
             self.game_height, self.game_width, self.msg_height, self.status_width)
-        self.game_win.box()
+        #self.game_win.box()
         self.game_win.refresh()
 
         self.status_win = curses.newwin(
@@ -61,9 +61,11 @@ class GameUI():
         self.status_win.box()
         self.status_win.refresh()
 
-        self.pad_height = 80
-        self.pad_width = 180
-        self.pad = curses.newpad(self.pad_height, self.pad_width)
+        self.pad_height = self.game_height + 10
+        self.pad_width = self.game_width + 10
+        self.pad_ul_x = 0
+        self.pad_ul_y = 0
+        self.pad = curses.newpad(self.pad_height+1, self.pad_width+1)
         curses.doupdate()
         self.pad.touchwin()
         self.clear()
@@ -106,45 +108,80 @@ class GameUI():
     def get_input(self):
         return self.screen.getch()
 
-    def fill_pad(self):
-        chars = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ]
-        for y in range(self.pad_height):
-            for x in range(self.game_width):
-                c = chars[((y*self.game_width)+x) % len(chars)]
-                self.pad.addstr(y,x,c, curses.color_pair(1))
+    def center_on(self, y, x):
+        # does the screen need to move?
+        screen_min_x = self.pad_ul_x
+        screen_max_x = self.pad_ul_x + self.game_width-1
+        screen_min_y = self.pad_ul_y 
+        screen_max_y = self.pad_ul_y + self.game_height-1
 
+        if x < screen_min_x + 10:
+            self.pad_ul_x = max(self.pad_ul_x - self.game_width//2, 0)
+        if x > screen_max_x - 10:
+            self.pad_ul_x = min(self.pad_width-self.game_width+1, self.pad_ul_x + self.game_width//2)
+        if y < screen_min_y + 10:
+            self.pad_ul_y = max(self.pad_ul_y - self.game_height//2, 0)
+        if y > screen_max_y - 10:
+            self.pad_ul_y = min(self.pad_height-self.game_height+1, self.pad_ul_y + self.game_height//2)
+
+        self.pad.refresh(self.pad_ul_y, self.pad_ul_x,
+           self.game_start_y, self.game_start_x,
+           self.game_start_y + self.game_height-1,
+           self.game_start_x + self.game_width-1)
+        curses.doupdate()
+        self.pad.touchwin()
+
+#----------------------------------------------------------------
+# NOTES:
+#   1. BUG - can't write to last pos in pad
+#   2. need to differentiate between background and foreground
+#       - restore the background on move
+#       - GameEngine::get_tile()
+#   3. where does the game engine live?
+#       - UI Has a game engine
+#---------------------------------------------------------------- 
 
 def main(stdscr):
+    import dungeon_gen
+    import random
     UI = GameUI(stdscr)
-    chars = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ]
-    for y in range(UI.game_height):
-        for x in range(UI.game_width):
-            c = chars[((y*UI.game_width)+x) % len(chars)]
-            UI.chars[y][x] = c
-    UI.PutChar(20,20, 'Q', curses.color_pair(2))
+    MAP = dungeon_gen.DungeonGenerator(UI.pad_width-1, UI.pad_height-1)
+    
+    for y in range(MAP.grid.height):
+        for x in range(MAP.grid.width):
+            UI.PutChar(y,x,MAP.grid[y,x], curses.color_pair(1))
 
-    ul_x = 0;
-    ul_y = 0;
+    #-------------------------------------
+    # Find an open sqare to start on
+    #-------------------------------------
+    this_y = 0
+    this_x = 0
+    while True:
+        this_y = random.randint(1, MAP.grid.height-1)
+        this_x = random.randint(1, MAP.grid.width-1)
+        if MAP.grid[this_y, this_x] == ' ':
+            break
 
-    UI.pad.refresh(ul_y, ul_x,
-               UI.game_start_y, UI.game_start_x,
-               UI.game_start_y + UI.game_height-2,
-               UI.game_start_x + UI.game_width-2)
-    curses.doupdate()
-    UI.pad.touchwin()
+    UI.center_on(this_y, this_x)
 
     while True:
         keypress = UI.get_input()
         if keypress in arrow_offsets:
-            ul_x = min(max(0,ul_x + arrow_offsets[keypress][0]), UI.pad_width-1)
-            ul_y = min(max(0,ul_y + arrow_offsets[keypress][1]), UI.pad_height-1)
-            #print (ul_x, ul_y, UI.pad_width, UI.pad_height)
-            UI.pad.refresh(ul_y, ul_x,
-               UI.game_start_y, UI.game_start_x,
-               UI.game_start_y + UI.game_height-2,
-               UI.game_start_x + UI.game_width-2)
-            curses.doupdate()
-            UI.pad.touchwin()
+            # calc no position
+            new_x =  min(max(0,this_x + arrow_offsets[keypress][0]), UI.pad_width-1)
+            new_y = min(max(0,this_y + arrow_offsets[keypress][1]), UI.pad_height-1)
+
+            if MAP.grid[new_y, new_x] not in [' ', '.', '+']:
+                continue
+
+            # clear old position
+            UI.PutChar(this_y, this_x, MAP.grid[this_y, this_x], curses.color_pair(1))
+
+            this_x, this_y = new_x, new_y
+
+            UI.PutChar(this_y, this_x, '@', curses.color_pair(1))
+            UI.center_on(this_y, this_x)
+
         elif keypress in [ord('q'),ord('Q')]:
             return
         else:
