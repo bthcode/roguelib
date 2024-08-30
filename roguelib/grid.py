492f26aaa2bf6ee969d10e8cf7830eacbbd3e7e9
Brian Hone <brian.hone@str.us>
2024-08-30 09:21:11 -0400

    upgrading the grid

--- roguelib/grid.py
+++ roguelib/grid.py
@@ -1,3 +1,9 @@
+from typing import Protocol, Iterator, Tuple, TypeVar, Optional, Iterable
+T = TypeVar('T')
+
+Location = TypeVar('Location')
+GridLocation = Tuple[int, int]
+
 class Grid:
     def __init__(self, height: int, width: int, val : str = '#' ):
         self.height : int = height
@@ -5,7 +11,7 @@ class Grid:
         self.data = []
         for i in range(height):
             self.data.append([val] * width)
-    def __setitem__(self, row_col, val):
+    def __setitem__(self, row_col, val : str):
         row, col = row_col
         if isinstance(row, slice):
             for r in range(row.start, row.stop):
@@ -21,29 +27,45 @@ class Grid:
             else:
                 self.data[row][col] = val
     def __getitem__(self, row_col):
+        ''' NOTE: Base interface to allow slices, etc '''
         row, col = row_col
+
+        # Double Slice
         if isinstance(row, slice) and isinstance(col, slice):
             rows = self.data[row]
             arr = [ x[col] for x in rows ]
             G = Grid(len(arr), len(arr[0]))
             G.data = arr
             return G
+        # Row Slice
         elif isinstance(row, slice):
             rows = self.data[row]
             arr = [ x[col] for x in rows ]
             G = Grid(len(arr), len(arr[0]))
             G.data = [arr]
             return G
+        # Col Slice
         elif isinstance(col, slice):
             arr = self.data[row][col]
             G = Grid(1, len(arr))
             G.data = [arr]
             return G
+        # Single tile accessor
         else:
             return self.data[row][col]
+
     def __str__(self):
         return "\n".join(["".join(row) for row in self.data])
-    def contains(self, vals):
+
+    def get_tile(self, loc:GridLocation):
+        ''' Primary means of fetching a tile '''
+        row, col = loc
+        if not self.in_bounds(loc):
+            raise ValueError("Loc out of bounds")
+        return self.data[row][col]
+
+    def contains(self, vals: Iterable[str]) -> bool:
+        ''' Does this grid contain anything in vals?  '''
         found = False
         for v in vals:
             for row in self.data:
@@ -51,12 +73,32 @@ class Grid:
                     return True
         return False
 
+    def in_bounds(self, loc: GridLocation) -> bool:
+        ''' Is this loc in the grid? '''
+        (y, x) = loc
+        return 0 <= x < self.width and 0 <= y < self.height
+    
+    def passable(self, loc: GridLocation) -> bool:
+        ''' Is this loc passable? '''
+        return self.get_tile(loc) not in ['+', '#']
+    
+    def neighbors(self, loc: GridLocation) -> Iterator[GridLocation]:
+        ''' What are the in-bounds neighbords of this loc? '''
+        (y, x) = loc
+        neighbors = [(x+1, y), (x-1, y), (x, y-1), (x, y+1)] # E W N S
+        # see "Ugly paths" section for an explanation:
+        if (x + y) % 2 == 0: neighbors.reverse() # S N W E
+        results = filter(self.in_bounds, neighbors)
+        results = filter(self.passable, results)
+        return results
+
+
 if __name__ == "__main__":
     G = Grid(5,6, '#')
     print ('Orig:')
     print (G)
     print ('Set val')
-    G[0,0] = 'b'
+    G[(0,0)] = 'b'
     print (G)
     print ('\nSingle row, col slice')
     G[1,2:5] = 'c'
@@ -79,3 +121,9 @@ if __name__ == "__main__":
     print (f"G[2:4,2:4].contains['a']: {G[2:4,2:4].contains(['a'])}")
     print (f"G[2:4,2:4].contains['#']: {G[2:4,2:4].contains(['#'])}")
 
+    print (f'G.in_bound((3,3)) = {G.in_bounds((3,3))}')
+    print (f'G.in_bound((10,3)) = {G.in_bounds((10,3))}')
+
+    print (f'G.neighbors((2,2)) = {list(G.neighbors((2,2)))}')
+    
+    print (f'G.get_tile((3,3)) = {G.get_tile((3,3))}')
